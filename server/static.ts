057c429e148be710247ff301d9406ecf94ed07d6
Bobbindale1 <161269319+Bobbindale1@users.noreply.github.com>
2026-01-02 21:45:56 +0000

    Create new path dist/public

--- server/static.ts
+++ server/static.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(process.cwd(), "client/dist");
+  const distPath = path.resolve(process.cwd(), "dist/public");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
